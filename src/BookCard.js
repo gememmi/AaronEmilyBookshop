f2438e0c7a23bd87b40ea0327e5db4fdfcc3cb0c
Aaron <ztoplin@yahoo.com>
2022-11-14 16:40:14 -0500

    rulfo/kawakami

--- src/BookCard.js
+++ src/BookCard.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-function BookCard(front, title) {
+function BookCard({front, title}) {
 
-    <li className="card_item">
-        <div className="card">
-            <img 
-                src= {front}
-                alt= {title}
-            />
-            <div className="card_title">{title}</div>
-        </div>
-    </li>
+    return (
+
+        <li className="card_item">
+            <div className="card">
+                <img 
+                    src= {front}
+                    alt= {title}
+                />
+                <div className="card_title">{title}</div>
+            </div>
+        </li>
+    )
 }
 
 export default BookCard
