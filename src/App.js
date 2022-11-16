e7884f398ab190baa5b5c299bcba19cdc60c47f1
Aaron <ztoplin@yahoo.com>
2022-11-16 10:49:39 -0500

    frankly out of date changes, emily's got the goods

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 
 import BooksContainer from './BooksContainer'; 
+import BookCard from './BookCard';
 import UserCart from './UserCart';
 import NavBar from './NavBar';
 import About from './About'
 
 function App() {
 
+
+    const [booksArray, setBooksArray] = useState([])
+    const [searchedItems, setSearchedItems] = useState("")
+
+    useEffect(() => {
+        fetch('http://localhost:3000/books')
+        .then(response => response.json())
+        .then(bookObjects => setBooksArray(bookObjects))
+    }, [])
+
+    const searchedItemsToDisplay = booksArray.filter((bookObject) => {
+
+        return bookObject.title.toLowerCase().includes(searchedItems.toLowerCase())
+
+        || 
+
+        bookObject.author.toLowerCase().includes(searchedItems.toLowerCase())
+
+        || 
+
+        bookObject.genre.toLowerCase().includes(searchedItems.toLowerCase())
+
+    })
+
+    const allBookCards = searchedItemsToDisplay.map(bookObject => {
+        return (
+            <BookCard 
+            key={bookObject.id} 
+            bookObject={bookObject}
+            title={bookObject.title} 
+            author={bookObject.author}
+            genre={bookObject.genre}
+            front={bookObject.front}
+            back={bookObject.back}
+            pages={bookObject.pages}
+            firstPublished={bookObject.firstPublished}
+            />
+
+        )
+    })
+
+    function handleSearch(event) {
+          setSearchedItems(event.target.value)
+      }
+
   return (
     <div className="App">
-      <BooksContainer />
+      <BooksContainer 
+      allBookCards={allBookCards}
+      handleSearch={handleSearch}
+      />
     </div>
   );
 }
--- src/BooksContainer.js
+++ src/BooksContainer.js
@@ -4,38 +4,15 @@ import SearchBar from './SearchBar';
 import UserCart from './UserCart';
 import GiftCard from './GiftCard';
 
-function BooksContainer() {
+function BooksContainer({allBookCards, handleSearch}) {
 
-    const [booksArray, setBooksArray] = useState([])
-
-    useEffect(() => {
-        fetch('http://localhost:3000/books')
-        .then(response => response.json())
-        .then(bookObjects => setBooksArray(bookObjects))
-    }, [])
-
-    const allBookCards = booksArray.map(bookObject => {
-        return (
-            <BookCard 
-            key={bookObject.id} 
-            bookObject={bookObject}
-            title={bookObject.title} 
-            author={bookObject.author}
-            genre={bookObject.genre}
-            front={bookObject.front}
-            back={bookObject.back}
-            pages={bookObject.pages}
-            firstPublished={bookObject.firstPublished}
-            />
-
-        )
-    })
-
-    // console.log(allBookCards)
+   
 
     return (
         <div>
-            {/* <SearchBar /> */}
+            <SearchBar 
+            handleSearch={handleSearch}
+            />
             <ul className="cards">
                 { allBookCards }
             </ul>
